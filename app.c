e3892cee413d9b33c091d811ae9ecbd641100569
kammradt <vinicius.kammradt1@gmail.com>
2019-08-01 15:35:49 -0300

    :sparkles: Using different type of variables

--- app.c
+++ app.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-  int age;
-  printf("Please insert your age: ");
-  scanf("%d", &age);
+  int a = 10;
+  float b = 20.5;
+  double c = 12.5;
+  printf("int size: %ld bytes. \n", sizeof(a));
+  printf("float size: %lu bytes. \n", sizeof(b));
+  printf("double size:  %lu bytes. \n", sizeof(c));
 
-  printf("Age typed: %d \n", age);
+  bool t = true;
+  bool f = false;
+  printf("True boolean value: %d \n", t);
+  printf("False boolean value: %d \n", f);
+  printf("True boolean value size: %ld \n", sizeof(t));
+  printf("False boolean value size: %ld \n", sizeof(f));
+
   return 0;
 }
