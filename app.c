c3eebfc2a2d15b212ceb30c1ea2b12601f1cf589
kammradt <vinicius.kammradt1@gmail.com>
2019-08-02 07:40:41 -0300

    :sparkles: Using do while

--- app.c
+++ app.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
-  int ages[] = {10, 12, 18};
-
-  int length = 3; // Hardcoded
-  int calculatedLength = sizeof(ages) / sizeof(ages[0]);
-
-  for (int i = 0; i < calculatedLength; i++) {
-    printf("Agee in position %d is %d \n", i, ages[i]);
-  }
+  short int inputInt;
+  do {
+    printf("Enter a number between 0 and 10: ");
+    scanf("%hd", &inputInt);
+  } while (inputInt <= 0 || inputInt >= 10 );
 
   return 0;
 }
