2598ae2c2ed7ea1599b4faefa60b7aa2062993e2
kammradt <vinicius.kammradt1@gmail.com>
2019-08-02 17:46:51 -0300

    :sparkles: Using <string.h> features

--- app.c
+++ app.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  int studentGrades[][4] = {
-    {6, 7, 10, 3},
-    {9, 3, 5, 2},
-    {7, 10, 8, 6},
-  };
-  int length_columns = sizeof(studentGrades[0][0]);
-  int length_rows = (sizeof(studentGrades) / length_columns) / sizeof(studentGrades[0][0]);
+  char name[9];
+  printf("Insert your name: ");
+  scanf("%8s", name);
 
-  for (int i = 0; i < length_rows; i++) {
-    for (int j = 0; j < length_columns; j++) {
-      printf("%d ", studentGrades[i][j]);
-    }
-    printf("\n");
+  int counter = 0;
+  while (name[counter] != '\0') {
+    counter++;
   }
+  printf("Name length: %d \n", counter);
+  printf("Name length: %ld \n", strlen(name));
+
+  if (strcmp(name, "vini") == 0) {
+    printf("You typed 'vini' \n");
+  } else {
+    printf("You do not typed 'vini' \n");
+  }
+
+  strcmp(name, "vini") == 0 ? printf("You typed 'vini' \n") : printf("You do not typed 'vini' \n");
 
   return 0;
 }
