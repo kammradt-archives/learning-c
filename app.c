a7470729fc3822ac464145d9ffac2916b23ddcfa
kammradt <vinicius.kammradt1@gmail.com>
2019-08-02 19:53:31 -0300

    :sparkles: Creating recursive functions

--- app.c
+++ app.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-int square(int number) {
-  return number * number;
-}
-
-int cube(int number) {
-  return number * number * number;
-}
-
-int power(int number, int exponent) {
-  int result = 1;
-  for (int i = 0; i < exponent; i++) {
-    result *= number;
+int recursivePower(int number, int exponent) {
+  if (exponent == 0) {
+    return 1;
   }
-  return result;
+  return number * recursivePower(number, exponent-1);
 }
 
 int main() {
-  printf("Result: %d \n", square(4));
-  printf("Result: %d \n", cube(4));
-  printf("Result: %d \n", power(4, 3));
+  printf("Result: %d \n", recursivePower(4, 3));
   return 0;
 }
 
