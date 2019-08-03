7cde89b512ddfdefd1640bd603de2ffc0d5dc62f
kammradt <vinicius.kammradt1@gmail.com>
2019-08-02 23:40:20 -0300

    :sparkles: Introduction to pointers

--- app.c
+++ app.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
-#define MAX_LENGTH 50
+
+void square(int *pointer) {
+  *pointer = *pointer * *pointer;
+}
 
 int main() {
-  printf("Insert your complete name: ");
-  char name[MAX_LENGTH];
-  fgets(name, MAX_LENGTH, stdin);
-  printf("Name: %s", name);
+  int number = 100;
+  int *pointer = &number; //Location of 'number'
+  printf("Number: %d \n", number);
+  printf("Pointer: %d \n\n", *pointer);
+
+  number = 200;
+  printf("Number: %d \n", number);
+  printf("Pointer: %d \n\n", *pointer);
+
+  *pointer = 300;
+  printf("Number: %d \n", number);
+  printf("Pointer: %d \n\n", *pointer);
+
+  int newNumber =  1000;
+  pointer = &newNumber;
+
+  printf("Number: %d \n", number);
+  printf("New Number: %d \n", newNumber);
+  printf("Pointer: %d \n\n", *pointer);
+
+  int value = 5;
+  square(&value);
+  printf("5x5: %d \n", value);
 
   return 0;
 }
