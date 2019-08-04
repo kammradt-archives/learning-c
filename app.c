ebe1cf35e0d059598edb9e209be1c0a1995d46cd
kammradt <vinicius.kammradt1@gmail.com>
2019-08-04 01:43:31 -0300

    :sparkles: Creating a fake database with structs

--- app.c
+++ app.c
@@ -1,41 +1,89 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-struct person {
-  int age;
-  int salary;
+struct Name {
+  char first_name[50];
+  char last_name[50];
+  char nickname[50];
 };
 
-struct address {
-  int houseNumber;
-  char street[30];
+struct Address {
+  char country[30];
+  char state[30];
+  char city[30];
+  char street_name[100];
+  int house_number;
+  int zipcode;
 };
 
-struct completeData {
-  int id;
-  struct person person;
-  struct address address;
+struct Hobby {
+  char name[30];
+  bool is_multiplayer;
+};
+
+struct DatabaseRecord {
+  struct Name name;
+  struct Address address;
+  struct Hobby *hobbies[3];
 };
 
 int main() {
-  struct person vini = { 19, 800 };
-  printf("The age is: %d, and the salary: %d. \n", vini.age, vini.salary);
-
-  struct address house = { 130, "Guilherme Kock" };
-  printf("The house number is: %d, and the street: %s. \n\n", house.houseNumber, house.street);
-
-  struct completeData completeObject= {
-    123,
-    {18, 800},
-    {130, "Guilherme Kock"},
+  struct Name user = {
+    "Vinicius",
+    "Kammradt",
+    "kamm"
   };
 
-  printf("Lets print a complete object w/ struct. \n");
-  printf("The age is: %d, the salary is: %d. \nThe house number is: %d and the street name is: %s. The ID is: %d. \n",
-  completeObject.person.age,
-  completeObject.person.salary,
-  completeObject.address.houseNumber,
-  completeObject.address.street,
-  completeObject.id);
+  struct Address address = {
+    "Brazil",
+    "SC",
+    "Joinville",
+    "Street 1",
+    199,
+    89223123
+  };
+
+  struct Hobby hobby1 = {
+    "Soccer",
+    true
+  };
+  struct Hobby hobby2 = {
+    "Draw",
+    false
+  };
+  struct Hobby hobby3 = {
+    "Fifa",
+    true
+  };
+
+  struct Hobby hobbies[] = {
+    hobby1, hobby2, hobby3
+  };
+
+  struct DatabaseRecord record = {
+    user,
+    address,
+    hobbies
+  };
+
+  printf("First name: %s\nLast name: %s\nNickname: %s\n\n",
+  record.name.first_name,
+  record.name.last_name,
+  record.name.nickname);
+
+  printf("Country: %s\nState: %s\nCity: %s\nStreet name: %s\nHouse number: %d\nZipcode: %d\n\n",
+  record.address.country,
+  record.address.state,
+  record.address.city,
+  record.address.street_name,
+  record.address.house_number,
+  record.address.zipcode);
+
+  for (int i = 0; i < 2; i++) {
+    printf("Hobby: %s\nCan play with friends?: ", hobbies[i].name);
+    printf(hobbies[i].is_multiplayer ? "Yes!\n\n" : "No!\n\n");
+  }
+
   return 0;
 }
 
