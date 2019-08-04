a0080427cd101367a27b4e3d3d15744139584e0e
kammradt <vinicius.kammradt1@gmail.com>
2019-08-04 13:05:21 -0300

    :sparkles: Using dynamic memory allocation

--- app.c
+++ app.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int increment(){
-  int static x = 0;
-  x++;
-  printf("%d\n", x);
-}
-
 int main() {
-  increment();
-  increment();
-  increment();
+  printf("How many 'ints' do you want? -> ");
+  int size;
+  scanf("%d", &size);
+
+  int *my_pointer = malloc(size * sizeof(int));
+
+  if(my_pointer==0) {
+    printf("Invalid pointer. Error allocating memory. \n\n");
+    return -1;
+  } else {
+    for (int i = 0; i < size; i++) {
+      printf("Type the %d element: ", i);
+      scanf("%d", &my_pointer[i]);
+    }
+
+    printf("The array: \n\n");
+    for (int i = 0; i < size; i++) {
+      printf("The %d element: %d\n", i, my_pointer[i]);
+    }
+  }
+  free(my_pointer);
   return 0;
 }
 
