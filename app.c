8e33425a5f2828f8b5d396e4ee1a06d253fa418f
kammradt <vinicius.kammradt1@gmail.com>
2019-08-03 22:32:19 -0300

    :sparkles: Using structs

--- app.c
+++ app.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 
-void square(int *pointer) {
-  *pointer = *pointer * *pointer;
-}
+struct person {
+  int age;
+  int salary;
+};
+
+struct address {
+  int houseNumber;
+  char street[30];
+};
+
+struct completeData {
+  int id;
+  struct person person;
+  struct address address;
+};
 
 int main() {
-  int number = 100;
-  int *pointer = &number; //Location of 'number'
-  printf("Number: %d \n", number);
-  printf("Pointer: %d \n\n", *pointer);
+  struct person vini = { 19, 800 };
+  printf("The age is: %d, and the salary: %d. \n", vini.age, vini.salary);
 
-  number = 200;
-  printf("Number: %d \n", number);
-  printf("Pointer: %d \n\n", *pointer);
+  struct address house = { 130, "Guilherme Kock" };
+  printf("The house number is: %d, and the street: %s. \n\n", house.houseNumber, house.street);
 
-  *pointer = 300;
-  printf("Number: %d \n", number);
-  printf("Pointer: %d \n\n", *pointer);
+  struct completeData completeObject= {
+    123,
+    {18, 800},
+    {130, "Guilherme Kock"},
+  };
 
-  int newNumber =  1000;
-  pointer = &newNumber;
-
-  printf("Number: %d \n", number);
-  printf("New Number: %d \n", newNumber);
-  printf("Pointer: %d \n\n", *pointer);
-
-  int value = 5;
-  square(&value);
-  printf("5x5: %d \n", value);
-
+  printf("Lets print a complete object w/ struct. \n");
+  printf("The age is: %d, the salary is: %d. \nThe house number is: %d and the street name is: %s. The ID is: %d. \n",
+  completeObject.person.age,
+  completeObject.person.salary,
+  completeObject.address.houseNumber,
+  completeObject.address.street,
+  completeObject.id);
   return 0;
 }
 
